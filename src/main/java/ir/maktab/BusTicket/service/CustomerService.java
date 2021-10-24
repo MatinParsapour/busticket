2e935628f566487fb6253df2f06a83ab60861630
Matin <matin.parsapour.iam@gmail.com>
2021-10-24 19:44:12 +0330

    create CustomerService and CustomerServiceImpl

---
+++ src/main/java/ir/maktab/BusTicket/service/CustomerService.java
@@ -0,0 +1,12 @@
+package ir.maktab.BusTicket.service;
+
+import ir.maktab.BusTicket.entities.Customer;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface CustomerService {
+
+    void addCustomer(Customer customer);
+
+    Customer getCustomer(Customer customer);
+}
---
+++ src/main/java/ir/maktab/BusTicket/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,24 @@
+package ir.maktab.BusTicket.service.impl;
+
+import ir.maktab.BusTicket.entities.Customer;
+import ir.maktab.BusTicket.repository.CustomerRepository;
+import ir.maktab.BusTicket.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public void addCustomer(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public Customer getCustomer(Customer customer) {
+        return customerRepository.findByUserNameAndPassword(customer.getUserName(),customer.getPassword());
+    }
+}
