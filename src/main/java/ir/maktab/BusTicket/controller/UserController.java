d6f10877280bae03b143d769d7c113a6ecd773c1
Matin <matin.parsapour.iam@gmail.com>
2021-10-24 19:49:37 +0330

    update userController

--- src/main/java/ir/maktab/BusTicket/controller/UserController.java
+++ src/main/java/ir/maktab/BusTicket/controller/UserController.java
@@ -1,13 +1,17 @@
 package ir.maktab.BusTicket.controller;
 
+import ir.maktab.BusTicket.entities.Customer;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/form")
-    public String signUpOrLogIn(){
+    public String signUpOrLogIn(Model model){
+        model.addAttribute("customer",new Customer());
         return "Form";
     }
+
 }
