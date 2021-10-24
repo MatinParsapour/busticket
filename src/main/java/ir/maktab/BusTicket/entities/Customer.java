874f6d0b4a34856622164acd0930070d3e6af321
Matin <matin.parsapour.iam@gmail.com>
2021-10-24 10:38:36 +0330

    create customer

---
+++ src/main/java/ir/maktab/BusTicket/entities/Customer.java
@@ -0,0 +1,15 @@
+package ir.maktab.BusTicket.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class Customer extends User{
+}
