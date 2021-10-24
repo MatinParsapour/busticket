978d4dd24e77f41737ddcd138f9c757f094f0924
Matin <matin.parsapour.iam@gmail.com>
2021-10-24 10:39:31 +0330

    create user controller

---
+++ src/main/java/ir/maktab/BusTicket/controller/UserController.java
@@ -0,0 +1,13 @@
+package ir.maktab.BusTicket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/form")
+    public String signUpOrLogIn(){
+        return "Form";
+    }
+}
