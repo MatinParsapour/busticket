348ed3c43041d2f5e19620183c4ae6f2d2f41b28
Matin <matin.parsapour.iam@gmail.com>
2021-10-25 10:49:21 +0330

    update TicketRepository to find all ticket base on an order

--- src/main/java/ir/maktab/BusTicket/repository/TicketRepository.java
+++ src/main/java/ir/maktab/BusTicket/repository/TicketRepository.java
@@ -3,5 +3,9 @@ package ir.maktab.BusTicket.repository;
 import ir.maktab.BusTicket.entities.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket,Long> {
+
+    List<Ticket> findAllByOrderByDepartureDateAsc();
 }
