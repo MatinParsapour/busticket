62e47109dcf8a1902de0e756d10713fb6e9b7ba4
Matin <matin.parsapour.iam@gmail.com>
2021-10-25 10:48:27 +0330

    update main controller to get all tickets

--- src/main/java/ir/maktab/BusTicket/controller/MainController.java
+++ src/main/java/ir/maktab/BusTicket/controller/MainController.java
@@ -1,14 +1,25 @@
 package ir.maktab.BusTicket.controller;
 
 
+import ir.maktab.BusTicket.entities.Ticket;
+import ir.maktab.BusTicket.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    private TicketService ticketService;
+
     @GetMapping("/")
-    public String main(){
+    public String main(Model model){
+        List<Ticket> tickets = ticketService.getAllTickets();
+        model.addAttribute("tickets",tickets);
         return "Home";
     }
 }
