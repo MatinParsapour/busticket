8247c5a308917246225b7c2b597eb69897a98a19
Matin <matin.parsapour.iam@gmail.com>
2021-10-26 15:26:34 +0330

    implement methods of History Service

--- src/main/java/ir/maktab/BusTicket/service/impl/HistoryServiceImpl.java
+++ src/main/java/ir/maktab/BusTicket/service/impl/HistoryServiceImpl.java
@@ -1,12 +1,15 @@
 package ir.maktab.BusTicket.service.impl;
 
 
+import ir.maktab.BusTicket.entities.Customer;
 import ir.maktab.BusTicket.entities.History;
 import ir.maktab.BusTicket.repository.HistoryRepository;
 import ir.maktab.BusTicket.service.HistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 @Service
@@ -27,5 +30,20 @@ public class HistoryServiceImpl implements HistoryService {
             travelNum+=random.nextInt(9);
         }
         return travelNum;
+    }
+
+    @Override
+    public List<History> getAllCustomerHistory(Customer customer) {
+        return historyRepository.findByCustomer(customer);
+    }
+
+    @Override
+    public Optional<History> getCustomerHistory(long id) {
+        return historyRepository.findById(id);
+    }
+
+    @Override
+    public void deleteHistory(History history) {
+        historyRepository.delete(history);
     }
 }
