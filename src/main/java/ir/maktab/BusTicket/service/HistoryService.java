630dc4ccb2414b87c5ce3d8113dc94f7ae92c819
Matin <matin.parsapour.iam@gmail.com>
2021-10-26 09:49:42 +0330

    Update HistoryService and HistoryServiceImpl to create travel num and add it to database

--- src/main/java/ir/maktab/BusTicket/service/HistoryService.java
+++ src/main/java/ir/maktab/BusTicket/service/HistoryService.java
@@ -1,4 +1,10 @@
 package ir.maktab.BusTicket.service;
 
+import ir.maktab.BusTicket.entities.History;
+
 public interface HistoryService {
+
+    void addHistory(History history);
+
+    String createTravelNum();
 }
--- src/main/java/ir/maktab/BusTicket/service/impl/HistoryServiceImpl.java
+++ src/main/java/ir/maktab/BusTicket/service/impl/HistoryServiceImpl.java
@@ -1,13 +1,31 @@
 package ir.maktab.BusTicket.service.impl;
 
 
+import ir.maktab.BusTicket.entities.History;
 import ir.maktab.BusTicket.repository.HistoryRepository;
+import ir.maktab.BusTicket.service.HistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
-public class HistoryServiceImpl {
+public class HistoryServiceImpl implements HistoryService {
 
     @Autowired
     private HistoryRepository historyRepository;
+
+    public void addHistory(History history){
+        historyRepository.save(history);
+    }
+
+    @Override
+    public String createTravelNum() {
+        String travelNum = "";
+        Random random = new Random();
+        for(int i = 0 ; i<6;i++){
+            travelNum+=random.nextInt(9);
+        }
+        return travelNum;
+    }
 }
