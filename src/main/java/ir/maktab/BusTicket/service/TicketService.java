f028b2cc62bc21db45320909ae4ba5dfaf752f84
Matin <matin.parsapour.iam@gmail.com>
2021-10-26 09:51:20 +0330

    Update TicketService and TicketServiceImpl to get ticket with id

--- src/main/java/ir/maktab/BusTicket/service/TicketService.java
+++ src/main/java/ir/maktab/BusTicket/service/TicketService.java
@@ -3,8 +3,11 @@ package ir.maktab.BusTicket.service;
 import ir.maktab.BusTicket.entities.Ticket;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TicketService {
 
     List<Ticket> getAllTickets();
+
+    Optional<Ticket> getTicket(long id);
 }
--- src/main/java/ir/maktab/BusTicket/service/impl/TicketServiceImpl.java
+++ src/main/java/ir/maktab/BusTicket/service/impl/TicketServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TicketServiceImpl implements TicketService {
@@ -17,5 +18,10 @@ public class TicketServiceImpl implements TicketService {
     @Override
     public List<Ticket> getAllTickets() {
         return ticketRepository.findAllByOrderByDepartureDateAsc();
+    }
+
+    @Override
+    public Optional<Ticket> getTicket(long id) {
+        return ticketRepository.findById(id);
     }
 }
