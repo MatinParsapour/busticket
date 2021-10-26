7ce9c09f5dba121470c0b2f8fefc3c184812ee24
Matin <matin.parsapour.iam@gmail.com>
2021-10-26 09:48:17 +0330

    update relationship between history and ticket and customer

--- src/main/java/ir/maktab/BusTicket/entities/History.java
+++ src/main/java/ir/maktab/BusTicket/entities/History.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,36 +18,25 @@ import javax.persistence.Table;
 @NoArgsConstructor
 public class History extends BaseEntity<Long>{
     public static final String TABLE_NAME = "history_table";
-    private static final String TICKET_ID =  "ticket_id";
+    private static final String TICKET =  "ticket";
     private static final String NAME =  "name";
     private static final String GENDER =  "gender";
-    private static final String ORIGIN =  "origin";
-    private static final String DESTINATION =  "destination";
-    private static final String DEPARTURE_DATE =  "departure_date";
-    private static final String DEPARTURE_TIME =  "departure_time";
+    private static final String CUSTOMER =  "customer";
     private static final String TRAVEL_NUM =  "travel_num";
 
 
-    @Column(name = TICKET_ID)
-    private String ticketId;
+
+    @OneToOne
+    private Ticket ticketId;
+
+    @OneToOne
+    private Customer customer;
 
     @Column(name = NAME)
     private String name;
 
     @Column(name = GENDER)
     private String gender;
-
-    @Column(name = ORIGIN)
-    private String origin;
-
-    @Column(name = DESTINATION)
-    private String destination;
-
-    @Column(name = DEPARTURE_DATE)
-    private String departureDate;
-
-    @Column(name = DEPARTURE_TIME)
-    private String departureTime;
 
     @Column(name = TRAVEL_NUM)
     private String travelNum;
